3e49ad6bd6d4e6017f4ba8bd9f044e409dcefd71
Jose Manuel <jmdcarq89@gmail.com>
2021-04-01 19:23:51 +0200

    initial structure

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
-const contact = () => {
-  return <div>contact page</div>
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>contact page</h1>
+    </Layout>
+  )
 }
 
-export default contact
+export default Contact
