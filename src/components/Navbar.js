187977c0435ace3bfcf782b578bc8f9d3910801a
Jose Manuel <jmdcarq89@gmail.com>
2021-04-01 18:12:25 +0200

    navBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello world!!</div>
+  return (
+    <>
+      <Navbar />
+      <h1>hola</h1>
+    </>
+  )
 }
