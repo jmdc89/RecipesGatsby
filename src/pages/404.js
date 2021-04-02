b660c6ca88b9991eb2e962822adc7990572abc8a
Jose Manuel <jmdcarq89@gmail.com>
2021-04-02 11:40:11 +0200

    Error styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styled from "styled-components"
+
+const ErrorStyle = styled.main`
+  min-height: calc(100vh - (6rem + 4rem));
+  text-align: center;
+  padding-top: 3rem;
+
+  h1 {
+    font-size: 9rem;
+  }
+`
 
 const Error = () => {
   return (
     <Layout>
-      <h1>error page</h1>
+      <ErrorStyle>
+        <section>
+          <h1>404</h1>
+          <h3>page not found</h3>
+        </section>
+      </ErrorStyle>
     </Layout>
   )
 }
