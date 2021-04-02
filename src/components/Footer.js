598da7d8a6de95461976d66f494bed9ecd3624ea
Jose Manuel <jmdcarq89@gmail.com>
2021-04-02 11:35:09 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,38 @@
 import React from "react"
+import styled from "styled-components"
+
+const PageFooter = styled.footer`
+  text-align: center;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #222;
+  color: #fff;
+
+  h5 {
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+
+  span,
+  a {
+    color: #645cff;
+  }
+`
 
 const Footer = () => {
   return (
-    <footer>
-      <h2>hello from footer</h2>
-    </footer>
+    <PageFooter>
+      <p>
+        &copy; {new Date().getFullYear()}
+        <span>SimplyRecipes</span>. Built with{" "}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      </p>
+    </PageFooter>
   )
 }
 
