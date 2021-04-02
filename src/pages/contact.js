f868e1a07899458ec4cf58b9698edd3e8ea9e15a
Jose Manuel <jmdcarq89@gmail.com>
2021-04-02 20:07:53 +0200

    Contact styled

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,52 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styled from "styled-components"
+
+const Page = styled.main`
+  width: 90vw;
+  max-width: var(--max-width);
+  margin: 0 auto;
+  min-height: calc(100vh - (6rem + 4rem));
+`
+
+const ContactPage = styled.section`
+  display: grid;
+  gap: 2rem 3rem;
+  padding-bottom: 3rem;
+
+  @media screen and (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+  }
+`
+
+const ContactInfo = styled.article``
+
+const Form = styled.form`
+  width: 100%;
+  background: white;
+  border-radius: 0.25rem;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  padding: 2rem 2.5rem;
+`
+
+const FormRow = styled.div`
+  margin-bottom: 1rem;
+`
+
+const FeaturedRecipes = styled.section`
+  h5 {
+    text-align: center;
+    max-width: 100%;
+  }
+`
 
 const Contact = () => {
   return (
     <Layout>
-      <main className="page">
-        <section className="contact-page">
+      <Page>
+        <ContactPage>
           <article className="contact-info">
             <h3>Want To Get In Touch?</h3>
             <p>
@@ -19,11 +60,11 @@ const Contact = () => {
             </p>
           </article>
           <article>
-            <form className="form contact-form">
-              <div className="form-row">
+            <Form>
+              <FormRow>
                 <label htmlFor="name">your name</label>
                 <input type="text" name="name" id="name" />
-              </div>
+              </FormRow>
               <div className="form-row">
                 <label htmlFor="email">your email</label>
                 <input type="text" name="email" id="email" />
@@ -35,13 +76,13 @@ const Contact = () => {
               <button type="submit" className="btn block">
                 submit
               </button>
-            </form>
+            </Form>
           </article>
-        </section>
-        <section className="featured-recipes">
+        </ContactPage>
+        <FeaturedRecipes>
           <h5>Look at this Awesomesouce!</h5>
-        </section>
-      </main>
+        </FeaturedRecipes>
+      </Page>
     </Layout>
   )
 }
