47e48109003bcc057bc181353fc39d9d8d0533f2
Jose Manuel <jmdcarq89@gmail.com>
2021-04-03 23:06:39 +0200

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "jose manuel dominguez",
+    person: { name: "jose", age: 31 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "jose", age: 31 },
+      { name: "manuel", age: 32 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
